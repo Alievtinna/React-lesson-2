d646f6a2c6720b2f5d9725b4735f476a53f8e9be
Alla <allaabdulina705@gmail.com>
2025-07-17 23:18:16 +0500

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyComponent extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  // увеличение
+  increment = () => {
+    this.setState({ count: this.state.count + 5 });
+  };
+
+  // Уменьшение на 1
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  // Деление на 2
+  divideByTwo = () => {
+    this.setState({ count: this.state.count / 2 });
+  };
+
+  // Умножение на 2
+  multiplyByTwo = () => {
+    this.setState({ count: this.state.count * 2 });
+  };
+
+  // Сброс всех данных
+  reset = () => {
+    this.setState({ count: 0 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Счётчик: {this.state.count}</h2>
+        <button onClick={this.increment}>Увеличить</button>
+        <button onClick={this.decrement}>Уменьшить на 1</button>
+        <button onClick={this.divideByTwo}>Разделить на 2</button>
+        <button onClick={this.multiplyByTwo}>Умножить на 2</button>
+        <button onClick={this.reset}>Сброс</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default MyComponent;
